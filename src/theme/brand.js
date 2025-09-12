db9a6644e828ef276be8914a1cc7d1c6a9b2bc94
cancoretamero <juanjose.r@aisagroup.ca>
2025-09-12 20:47:57 +0200

    Create brand.js

---
+++ src/theme/brand.js
@@ -0,0 +1,9 @@
+export const brand = {
+  bg: '#0b0d0e',                   // antracita
+  glass: 'bg-black/30 backdrop-blur-xl',  // efecto glass oscuro
+  border: 'border-white/12',
+  dim: 'text-white/70',
+  text: 'text-white/90',
+  neonCyan: '#67e8f9',
+  neonOrange: '#ff9a62',
+}
